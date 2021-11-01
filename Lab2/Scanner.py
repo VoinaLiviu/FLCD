a7b03711a9cd7be7e981ed13787f340e411a8db5
VoinaLiviu <liviuvoina79@gmail.com>
2021-11-01 20:43:02 +0200

    Updated Lab2

--- Lab2/Scanner.py
+++ Lab2/Scanner.py
@@ -1,5 +1,7 @@
 import SymbolTable
+import re
 
+#0.5p
 class Scanner:
     def __init__(self):
         self.symbolTable = SymbolTable
@@ -8,3 +10,22 @@ class Scanner:
         self.tokens = ["if", "then", "for", "do", "while", ".", ";", ">", "<", "=", "<=", ">=", "!=", "(", ")", "int",
                        "char", "string", "float", "bool", "+", "-", "/", "*"]
         self.constants = []
+
+    def isIdentifier(self, token):
+        return re.match(r'[a-z]([a-zA-Z]|[0-9])*$', token) is not None
+
+    def isConstant(self, token):
+        return re.match(r'^(0|[+-]?[1-9][0-9]*)$|^\'.\'$|^\'.*\'$', token) is not None
+
+    def readProgram(self, fileName):
+        file = open(fileName, "r")
+
+        for line in file:
+            splittedLine = line.split(" ")
+            for element in splittedLine:
+                if element not in self.tokens:
+                    if self.isConstant(element):
+                        self.constants.append(element)
+                    if self.isIdentifier(element):
+                        self.identifiers.append(element)
+
